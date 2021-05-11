8aaf0e64ddeb8303921b4411698c062ed84b775a
SudhanshuVijay0 <78337703+SudhanshuVijay0@users.noreply.github.com>
2021-05-11 14:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,6 @@ model=pickle.load(pickle_in)
 dataset= pd.read_csv('dataset.csv')
 X = dataset.iloc[:,1:10].values
 
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'constant', fill_value='Female', verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 1:2])
-#Replacing missing data with the calculated mean value  
-X[:, 1:2]= imputer.transform(X[:, 1:2])  
 
 # Taking care of missing data
 #handling missing data (Replacing missing data with the mean value)  
@@ -34,27 +26,12 @@ X[:, 2:3]= imputer.transform(X[:, 2:3])
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values= np.NAN, strategy= 'mean', fill_value=None, verbose=1, copy=True)
 #Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 3:6])
+imputer = imputer.fit(X[:, 3:9])
 #Replacing missing data with the calculated mean value  
-X[:, 3:6]= imputer.transform(X[:, 3:6])
+X[:, 3:6]= imputer.transform(X[:, 3:9])
 
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'most_frequent', fill_value='none', verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 6:8])
-#Replacing missing data with the calculated mean value  
-X[:, 6:8]= imputer.transform(X[:, 6:8])
 
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'mean', fill_value=None, verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 8:9])
-#Replacing missing data with the calculated mean value  
-X[:, 8:9]= imputer.transform(X[:, 8:9])  
+  
 
 # Encoding Categorical data:
 # Encoding the Independent Variable
@@ -101,8 +78,7 @@ def main():
    </div>
    """
     st.markdown(html_temp,unsafe_allow_html=True)
-    st.header("Customer continue or not...")
-    Name = st.text_input('Enter name')
+    st.header("Customer continue or not...")   
     CreditScore = st.number_input('Insert credit score',100,1000)
     Geography= st.number_input('Insert 0 for France 1 for Spain',0,1)
     Gender = st.number_input('Insert 0 for Male 1 for Female ',0,1)
@@ -115,7 +91,7 @@ def main():
     
     result=""
     if st.button("Predict"):
-      result=predict_note_authentication(age,cp,trestbps,chol,fbs,Gender,Geography,restecg,thalach,exang,oldpeak,slope,ca,thal)
+      result=predict_note_authentication(CreditScore, Geography, Gender, Age, Tenure, Balance, HasCrCard, IsActiveMember, EstimatedSalary)
       st.success('Model has predicted {}'.format(result))
     if st.button("About"):
       st.subheader("Developed by Shruti Jain")
